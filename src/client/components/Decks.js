5729d418926dbf63af43dfcfc006599458d9e8d9
Nathanael Ligon <nathanaelligon@Nathanaels-MacBook-Air.local>
2016-06-10 11:44:51 -0700

    Implement search bar template in src/client/components/Decks.js issuing GET request for http://google.com as a placeholder

--- src/client/components/Decks.js
+++ src/client/components/Decks.js
@@ -1,12 +1,20 @@
 import React, { PropTypes } from 'react';
 import DeckItem from './DeckItem';
+// this.state = decks;
 
 const Decks = ({ decks }) => (
   <div className="container">
     <h4 className="center grey-text text-darken-4"> Decks </h4>
+    
+            <form id="tfnewsearch" method="get" action="http://www.google.com">
+            <p><input type="text" placeholder="Type your search here"/></p>
+            <p><input type="submit" value="search" class="tfbutton"/></p>  
+            </form>
+              <div class="tfclear"></div>
+              
     <div className="card-list">
       <div className="card-columns">
-        {decks.map((deck, idx) => <DeckItem key={idx} deck={deck} />)}
+        {decks.map((deck, idx) => r<DeckItem key={idx} deck={deck} />)}
       </div>
     </div>
   </div>
