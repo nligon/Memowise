7a57c9286823e5f566918bceab3c4911dcd4ec5b
Nathanael Ligon <nathanaelligon@Nathanaels-MacBook-Air.local>
2016-06-10 20:24:18 -0700

    Implement react component 'Search' in 'Decks.js' using component rendering to replace static html

---
+++ src/client/components/Search.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+
+class Search extends React.Component {
+  
+  render() {
+    return (
+      <div className="search-bar form-inline">
+      Hello, world!
+      </div>
+    );
+  }
+}
+
+export default Search;
