9af2760bff1e3e6a199d201cc0fd8314e8f2fb5c
Nathanael Ligon <nathanaelligon@Nathanaels-MacBook-Air.local>
2016-06-10 20:36:19 -0700

    Replace Search component's 'hello world' with google-pointing search bar'

--- src/client/components/Search.js
+++ src/client/components/Search.js
@@ -5,7 +5,12 @@ class Search extends React.Component {
   render() {
     return (
       <div className="search-bar form-inline">
-      Hello, world!
+        <form id="tfnewsearch" method="get" action="http://www.google.com">
+          <p><input type="text" placeholder="Type your search here"/></p>
+          <p><input type="submit" value="search" class="tfbutton"/></p>  
+        </form>
+        <div class="tfclear">
+        </div>      
       </div>
     );
   }
